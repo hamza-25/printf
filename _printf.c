c0dd9b645ad97de09a9a4feb3291a25d69121e27
hamza-25 <me677499@gmail.com>
2023-04-22 05:38:05 -0700

    _print function code

---
+++ _printf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - print function
+ * @format: given string
+ * Return: always 0
+*/
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list ptr;
+
+	va_start(ptr, format);
+	while (format && format[i])
+	{
+	if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
+	{
+		switch (format[i + 1])
+		{
+			case 'c':
+				printf("%c\n", va_arg(ptr, char *));
+				break;
+			case 's':
+				printf("%s\n", va_arg(ptr, char *));
+				break;
+		}
+	}
+	else
+	{
+	printf("%c", format[i]);
+	}
+	i++;
+}
