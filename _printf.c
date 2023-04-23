d8473de5730a5281b2ec59594980ac57b8f306c4
hamza-25 <me677499@gmail.com>
2023-04-23 17:48:19 +0000

    fixing pointer

--- _printf.c
+++ _printf.c
@@ -34,7 +34,7 @@ int _printf(const char *format, ...)
 		switch (format[i + 1])
 		{
 			case 'c':
-				buffer[ibuf] = va_arg(ptr, char *);
+				buffer[ibuf] = va_arg(ptr, int);
 				ibuf++;
 				i++;
 				break;
