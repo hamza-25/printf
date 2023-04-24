ef24504d3467d9e006470ba7b1518ed17e3097a0
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-24 14:11:20 +0000

    Update handle_buffer.c

--- handle_buffer.c
+++ handle_buffer.c
@@ -5,8 +5,8 @@
  * @buffer: buffer pointer
  * @c: charcter to concatenate
  * @ibuf: index of buffer pointer
- * Return: index of buffer pointer.
- */
+ * Return: index of buffer pointer
+*/
 unsigned int handl_buf(char *buffer, char c, unsigned int ibuf)
 {
 	buffer[ibuf] = c;
