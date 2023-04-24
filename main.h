00218659f0ff9f0888c1008cdfaab33203ba1da7
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-24 13:44:01 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-#include <stdarg.h>
-int _printf(const char *format, ...);
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+unsigned int handl_buf(char *buffer, char c, unsigned int ibuf)
+  
 #endif
