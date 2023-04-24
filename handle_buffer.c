ea491b39a16605de4171fcef180d54505c6cef05
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-04-24 14:12:35 +0000

    Update handle_buffer.c

--- handle_buffer.c
+++ handle_buffer.c
@@ -7,7 +7,7 @@
  * @ibuf: index of buffer pointer
  * Return: index of buffer pointer
 */
-unsigned int handl_buffer(char *buff, char c, unsigned int ibuf)
+unsigned int handle_buffer(char *buff, char c, unsigned int ibuf)
 {
 	buff[ibuf] = c;
 	ibuf++;
