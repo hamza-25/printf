69c78473ef0ad5432b6d153f61e38f6774046696
ouhaddou <ouhaddou7398@gmail.com>
2023-04-25 14:14:37 -0700

    _printf v2

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * **_printf - Printf function
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -52,7 +52,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * **print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
